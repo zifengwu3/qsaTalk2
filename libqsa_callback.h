714d39352bf9678c4dba01df930ff3dbdbe7d6f6
l <l@fishyuyu>
2015-12-31 22:31:10 +0800

    3

--- libqsa_callback.h
+++ libqsa_callback.h
@@ -1,12 +1,13 @@
+
 //CALLBACK Function
 typedef void ( * _cb_data)(void *, int, int);
 typedef void ( * _cb_info)(const char *, const char *, int);
 typedef void ( * _cb_status)(int, int);
 
 static struct _cb_function {
-    _cb_data cb_audio_data;         //回调声音参数
-    _cb_status cb_curr_status;      //当前呼叫状态
-    _cb_info cb_info;               //转发消息给其他服务器
+    _cb_data cb_audio_data;
+    _cb_status cb_curr_status;
+    _cb_info cb_info;
 };
 
 void set_cb_function(struct _cb_function * p);
