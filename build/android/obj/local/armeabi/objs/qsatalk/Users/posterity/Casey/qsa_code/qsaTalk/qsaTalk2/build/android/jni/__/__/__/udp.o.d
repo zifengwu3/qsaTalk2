dedf86f5292d9a830bef7ee8aa04aad85f9384af
Zhou Xiaomin <zxm123465@gmail.com>
2016-01-23 08:29:57 +0800

    H

---
+++ build/android/obj/local/armeabi/objs/qsatalk/Users/posterity/Casey/qsa_code/qsaTalk/qsaTalk2/build/android/jni/__/__/__/udp.o.d
@@ -0,0 +1,233 @@
+/Users/posterity/Casey/qsa_code/qsaTalk/qsaTalk2/build/android/obj/local/armeabi/objs/qsatalk//Users/posterity/Casey/qsa_code/qsaTalk/qsaTalk2/build/android/jni/__/__/__//udp.o: \
+ /Users/posterity/Casey/qsa_code/qsaTalk/qsaTalk2/build/android/jni/../../..//udp.c \
+ /Users/posterity/software/android-ndk-r10d/platforms/android-3/arch-arm/usr/include/stdio.h \
+ /Users/posterity/software/android-ndk-r10d/platforms/android-3/arch-arm/usr/include/sys/cdefs.h \
+ /Users/posterity/software/android-ndk-r10d/platforms/android-3/arch-arm/usr/include/sys/cdefs_elf.h \
+ /Users/posterity/software/android-ndk-r10d/platforms/android-3/arch-arm/usr/include/android/api-level.h \
+ /Users/posterity/software/android-ndk-r10d/platforms/android-3/arch-arm/usr/include/sys/types.h \
+ /Users/posterity/software/android-ndk-r10d/platforms/android-3/arch-arm/usr/include/stdint.h \
+ /Users/posterity/software/android-ndk-r10d/platforms/android-3/arch-arm/usr/include/sys/_types.h \
+ /Users/posterity/software/android-ndk-r10d/platforms/android-3/arch-arm/usr/include/machine/_types.h \
+ /Users/posterity/software/android-ndk-r10d/platforms/android-3/arch-arm/usr/include/sys/_wchar_limits.h \
+ /Users/posterity/software/android-ndk-r10d/platforms/android-3/arch-arm/usr/include/linux/posix_types.h \
+ /Users/posterity/software/android-ndk-r10d/platforms/android-3/arch-arm/usr/include/linux/stddef.h \
+ /Users/posterity/software/android-ndk-r10d/platforms/android-3/arch-arm/usr/include/linux/compiler.h \
+ /Users/posterity/software/android-ndk-r10d/platforms/android-3/arch-arm/usr/include/asm/posix_types.h \
+ /Users/posterity/software/android-ndk-r10d/platforms/android-3/arch-arm/usr/include/asm/types.h \
+ /Users/posterity/software/android-ndk-r10d/platforms/android-3/arch-arm/usr/include/linux/types.h \
+ /Users/posterity/software/android-ndk-r10d/platforms/android-3/arch-arm/usr/include/machine/kernel.h \
+ /Users/posterity/software/android-ndk-r10d/platforms/android-3/arch-arm/usr/include/sys/sysmacros.h \
+ /Users/posterity/software/android-ndk-r10d/platforms/android-3/arch-arm/usr/include/time.h \
+ /Users/posterity/software/android-ndk-r10d/platforms/android-3/arch-arm/usr/include/sys/time.h \
+ /Users/posterity/software/android-ndk-r10d/platforms/android-3/arch-arm/usr/include/linux/time.h \
+ /Users/posterity/software/android-ndk-r10d/platforms/android-3/arch-arm/usr/include/asm/siginfo.h \
+ /Users/posterity/software/android-ndk-r10d/platforms/android-3/arch-arm/usr/include/asm-generic/siginfo.h \
+ /Users/posterity/software/android-ndk-r10d/platforms/android-3/arch-arm/usr/include/netinet/in.h \
+ /Users/posterity/software/android-ndk-r10d/platforms/android-3/arch-arm/usr/include/endian.h \
+ /Users/posterity/software/android-ndk-r10d/platforms/android-3/arch-arm/usr/include/sys/endian.h \
+ /Users/posterity/software/android-ndk-r10d/platforms/android-3/arch-arm/usr/include/machine/endian.h \
+ /Users/posterity/software/android-ndk-r10d/platforms/android-3/arch-arm/usr/include/linux/socket.h \
+ /Users/posterity/software/android-ndk-r10d/platforms/android-3/arch-arm/usr/include/asm/socket.h \
+ /Users/posterity/software/android-ndk-r10d/platforms/android-3/arch-arm/usr/include/asm/sockios.h \
+ /Users/posterity/software/android-ndk-r10d/platforms/android-3/arch-arm/usr/include/linux/sockios.h \
+ /Users/posterity/software/android-ndk-r10d/platforms/android-3/arch-arm/usr/include/linux/uio.h \
+ /Users/posterity/software/android-ndk-r10d/platforms/android-3/arch-arm/usr/include/linux/in.h \
+ /Users/posterity/software/android-ndk-r10d/platforms/android-3/arch-arm/usr/include/asm/byteorder.h \
+ /Users/posterity/software/android-ndk-r10d/platforms/android-3/arch-arm/usr/include/linux/byteorder/little_endian.h \
+ /Users/posterity/software/android-ndk-r10d/platforms/android-3/arch-arm/usr/include/linux/byteorder/swab.h \
+ /Users/posterity/software/android-ndk-r10d/platforms/android-3/arch-arm/usr/include/linux/byteorder/generic.h \
+ /Users/posterity/software/android-ndk-r10d/platforms/android-3/arch-arm/usr/include/linux/in6.h \
+ /Users/posterity/software/android-ndk-r10d/platforms/android-3/arch-arm/usr/include/linux/ipv6.h \
+ /Users/posterity/software/android-ndk-r10d/platforms/android-3/arch-arm/usr/include/netinet/in6.h \
+ /Users/posterity/software/android-ndk-r10d/platforms/android-3/arch-arm/usr/include/sys/socket.h \
+ /Users/posterity/software/android-ndk-r10d/platforms/android-3/arch-arm/usr/include/errno.h \
+ /Users/posterity/software/android-ndk-r10d/platforms/android-3/arch-arm/usr/include/linux/errno.h \
+ /Users/posterity/software/android-ndk-r10d/platforms/android-3/arch-arm/usr/include/asm/errno.h \
+ /Users/posterity/software/android-ndk-r10d/platforms/android-3/arch-arm/usr/include/asm-generic/errno.h \
+ /Users/posterity/software/android-ndk-r10d/platforms/android-3/arch-arm/usr/include/asm-generic/errno-base.h \
+ /Users/posterity/software/android-ndk-r10d/platforms/android-3/arch-arm/usr/include/arpa/inet.h \
+ /Users/posterity/software/android-ndk-r10d/platforms/android-3/arch-arm/usr/include/unistd.h \
+ /Users/posterity/software/android-ndk-r10d/platforms/android-3/arch-arm/usr/include/sys/select.h \
+ /Users/posterity/software/android-ndk-r10d/platforms/android-3/arch-arm/usr/include/signal.h \
+ /Users/posterity/software/android-ndk-r10d/platforms/android-3/arch-arm/usr/include/limits.h \
+ /Users/posterity/software/android-ndk-r10d/platforms/android-3/arch-arm/usr/include/sys/limits.h \
+ /Users/posterity/software/android-ndk-r10d/platforms/android-3/arch-arm/usr/include/linux/limits.h \
+ /Users/posterity/software/android-ndk-r10d/platforms/android-3/arch-arm/usr/include/machine/internal_types.h \
+ /Users/posterity/software/android-ndk-r10d/platforms/android-3/arch-arm/usr/include/machine/limits.h \
+ /Users/posterity/software/android-ndk-r10d/platforms/android-3/arch-arm/usr/include/sys/syslimits.h \
+ /Users/posterity/software/android-ndk-r10d/platforms/android-3/arch-arm/usr/include/asm/page.h \
+ /Users/posterity/software/android-ndk-r10d/platforms/android-3/arch-arm/usr/include/string.h \
+ /Users/posterity/software/android-ndk-r10d/platforms/android-3/arch-arm/usr/include/malloc.h \
+ /Users/posterity/software/android-ndk-r10d/platforms/android-3/arch-arm/usr/include/asm/signal.h \
+ /Users/posterity/software/android-ndk-r10d/platforms/android-3/arch-arm/usr/include/asm-generic/signal.h \
+ /Users/posterity/software/android-ndk-r10d/platforms/android-3/arch-arm/usr/include/sys/ucontext.h \
+ /Users/posterity/software/android-ndk-r10d/platforms/android-3/arch-arm/usr/include/sys/user.h \
+ /Users/posterity/software/android-ndk-r10d/platforms/android-3/arch-arm/usr/include/asm/sigcontext.h \
+ /Users/posterity/software/android-ndk-r10d/platforms/android-3/arch-arm/usr/include/sys/sysconf.h \
+ /Users/posterity/software/android-ndk-r10d/platforms/android-3/arch-arm/usr/include/linux/capability.h \
+ /Users/posterity/software/android-ndk-r10d/platforms/android-3/arch-arm/usr/include/pathconf.h \
+ /Users/posterity/software/android-ndk-r10d/platforms/android-3/arch-arm/usr/include/semaphore.h \
+ /Users/posterity/software/android-ndk-r10d/platforms/android-3/arch-arm/usr/include/dirent.h \
+ /Users/posterity/software/android-ndk-r10d/platforms/android-3/arch-arm/usr/include/pthread.h \
+ /Users/posterity/software/android-ndk-r10d/platforms/android-3/arch-arm/usr/include/sched.h \
+ /Users/posterity/Casey/qsa_code/qsaTalk/qsaTalk2/build/android/jni/../../..//include/libqsa_common.h \
+ /Users/posterity/software/android-ndk-r10d/platforms/android-3/arch-arm/usr/include/stdlib.h \
+ /Users/posterity/software/android-ndk-r10d/platforms/android-3/arch-arm/usr/include/alloca.h \
+ /Users/posterity/software/android-ndk-r10d/platforms/android-3/arch-arm/usr/include/strings.h \
+ /Users/posterity/software/android-ndk-r10d/platforms/android-3/arch-arm/usr/include/memory.h \
+ /Users/posterity/Casey/qsa_code/qsaTalk/qsaTalk2/build/android/jni/../../..//include/libqsa_def.h \
+ /Users/posterity/Casey/qsa_code/qsaTalk/qsaTalk2/build/android/jni/../../..//include/libqsa_callback.h
+
+/Users/posterity/software/android-ndk-r10d/platforms/android-3/arch-arm/usr/include/stdio.h:
+
+/Users/posterity/software/android-ndk-r10d/platforms/android-3/arch-arm/usr/include/sys/cdefs.h:
+
+/Users/posterity/software/android-ndk-r10d/platforms/android-3/arch-arm/usr/include/sys/cdefs_elf.h:
+
+/Users/posterity/software/android-ndk-r10d/platforms/android-3/arch-arm/usr/include/android/api-level.h:
+
+/Users/posterity/software/android-ndk-r10d/platforms/android-3/arch-arm/usr/include/sys/types.h:
+
+/Users/posterity/software/android-ndk-r10d/platforms/android-3/arch-arm/usr/include/stdint.h:
+
+/Users/posterity/software/android-ndk-r10d/platforms/android-3/arch-arm/usr/include/sys/_types.h:
+
+/Users/posterity/software/android-ndk-r10d/platforms/android-3/arch-arm/usr/include/machine/_types.h:
+
+/Users/posterity/software/android-ndk-r10d/platforms/android-3/arch-arm/usr/include/sys/_wchar_limits.h:
+
+/Users/posterity/software/android-ndk-r10d/platforms/android-3/arch-arm/usr/include/linux/posix_types.h:
+
+/Users/posterity/software/android-ndk-r10d/platforms/android-3/arch-arm/usr/include/linux/stddef.h:
+
+/Users/posterity/software/android-ndk-r10d/platforms/android-3/arch-arm/usr/include/linux/compiler.h:
+
+/Users/posterity/software/android-ndk-r10d/platforms/android-3/arch-arm/usr/include/asm/posix_types.h:
+
+/Users/posterity/software/android-ndk-r10d/platforms/android-3/arch-arm/usr/include/asm/types.h:
+
+/Users/posterity/software/android-ndk-r10d/platforms/android-3/arch-arm/usr/include/linux/types.h:
+
+/Users/posterity/software/android-ndk-r10d/platforms/android-3/arch-arm/usr/include/machine/kernel.h:
+
+/Users/posterity/software/android-ndk-r10d/platforms/android-3/arch-arm/usr/include/sys/sysmacros.h:
+
+/Users/posterity/software/android-ndk-r10d/platforms/android-3/arch-arm/usr/include/time.h:
+
+/Users/posterity/software/android-ndk-r10d/platforms/android-3/arch-arm/usr/include/sys/time.h:
+
+/Users/posterity/software/android-ndk-r10d/platforms/android-3/arch-arm/usr/include/linux/time.h:
+
+/Users/posterity/software/android-ndk-r10d/platforms/android-3/arch-arm/usr/include/asm/siginfo.h:
+
+/Users/posterity/software/android-ndk-r10d/platforms/android-3/arch-arm/usr/include/asm-generic/siginfo.h:
+
+/Users/posterity/software/android-ndk-r10d/platforms/android-3/arch-arm/usr/include/netinet/in.h:
+
+/Users/posterity/software/android-ndk-r10d/platforms/android-3/arch-arm/usr/include/endian.h:
+
+/Users/posterity/software/android-ndk-r10d/platforms/android-3/arch-arm/usr/include/sys/endian.h:
+
+/Users/posterity/software/android-ndk-r10d/platforms/android-3/arch-arm/usr/include/machine/endian.h:
+
+/Users/posterity/software/android-ndk-r10d/platforms/android-3/arch-arm/usr/include/linux/socket.h:
+
+/Users/posterity/software/android-ndk-r10d/platforms/android-3/arch-arm/usr/include/asm/socket.h:
+
+/Users/posterity/software/android-ndk-r10d/platforms/android-3/arch-arm/usr/include/asm/sockios.h:
+
+/Users/posterity/software/android-ndk-r10d/platforms/android-3/arch-arm/usr/include/linux/sockios.h:
+
+/Users/posterity/software/android-ndk-r10d/platforms/android-3/arch-arm/usr/include/linux/uio.h:
+
+/Users/posterity/software/android-ndk-r10d/platforms/android-3/arch-arm/usr/include/linux/in.h:
+
+/Users/posterity/software/android-ndk-r10d/platforms/android-3/arch-arm/usr/include/asm/byteorder.h:
+
+/Users/posterity/software/android-ndk-r10d/platforms/android-3/arch-arm/usr/include/linux/byteorder/little_endian.h:
+
+/Users/posterity/software/android-ndk-r10d/platforms/android-3/arch-arm/usr/include/linux/byteorder/swab.h:
+
+/Users/posterity/software/android-ndk-r10d/platforms/android-3/arch-arm/usr/include/linux/byteorder/generic.h:
+
+/Users/posterity/software/android-ndk-r10d/platforms/android-3/arch-arm/usr/include/linux/in6.h:
+
+/Users/posterity/software/android-ndk-r10d/platforms/android-3/arch-arm/usr/include/linux/ipv6.h:
+
+/Users/posterity/software/android-ndk-r10d/platforms/android-3/arch-arm/usr/include/netinet/in6.h:
+
+/Users/posterity/software/android-ndk-r10d/platforms/android-3/arch-arm/usr/include/sys/socket.h:
+
+/Users/posterity/software/android-ndk-r10d/platforms/android-3/arch-arm/usr/include/errno.h:
+
+/Users/posterity/software/android-ndk-r10d/platforms/android-3/arch-arm/usr/include/linux/errno.h:
+
+/Users/posterity/software/android-ndk-r10d/platforms/android-3/arch-arm/usr/include/asm/errno.h:
+
+/Users/posterity/software/android-ndk-r10d/platforms/android-3/arch-arm/usr/include/asm-generic/errno.h:
+
+/Users/posterity/software/android-ndk-r10d/platforms/android-3/arch-arm/usr/include/asm-generic/errno-base.h:
+
+/Users/posterity/software/android-ndk-r10d/platforms/android-3/arch-arm/usr/include/arpa/inet.h:
+
+/Users/posterity/software/android-ndk-r10d/platforms/android-3/arch-arm/usr/include/unistd.h:
+
+/Users/posterity/software/android-ndk-r10d/platforms/android-3/arch-arm/usr/include/sys/select.h:
+
+/Users/posterity/software/android-ndk-r10d/platforms/android-3/arch-arm/usr/include/signal.h:
+
+/Users/posterity/software/android-ndk-r10d/platforms/android-3/arch-arm/usr/include/limits.h:
+
+/Users/posterity/software/android-ndk-r10d/platforms/android-3/arch-arm/usr/include/sys/limits.h:
+
+/Users/posterity/software/android-ndk-r10d/platforms/android-3/arch-arm/usr/include/linux/limits.h:
+
+/Users/posterity/software/android-ndk-r10d/platforms/android-3/arch-arm/usr/include/machine/internal_types.h:
+
+/Users/posterity/software/android-ndk-r10d/platforms/android-3/arch-arm/usr/include/machine/limits.h:
+
+/Users/posterity/software/android-ndk-r10d/platforms/android-3/arch-arm/usr/include/sys/syslimits.h:
+
+/Users/posterity/software/android-ndk-r10d/platforms/android-3/arch-arm/usr/include/asm/page.h:
+
+/Users/posterity/software/android-ndk-r10d/platforms/android-3/arch-arm/usr/include/string.h:
+
+/Users/posterity/software/android-ndk-r10d/platforms/android-3/arch-arm/usr/include/malloc.h:
+
+/Users/posterity/software/android-ndk-r10d/platforms/android-3/arch-arm/usr/include/asm/signal.h:
+
+/Users/posterity/software/android-ndk-r10d/platforms/android-3/arch-arm/usr/include/asm-generic/signal.h:
+
+/Users/posterity/software/android-ndk-r10d/platforms/android-3/arch-arm/usr/include/sys/ucontext.h:
+
+/Users/posterity/software/android-ndk-r10d/platforms/android-3/arch-arm/usr/include/sys/user.h:
+
+/Users/posterity/software/android-ndk-r10d/platforms/android-3/arch-arm/usr/include/asm/sigcontext.h:
+
+/Users/posterity/software/android-ndk-r10d/platforms/android-3/arch-arm/usr/include/sys/sysconf.h:
+
+/Users/posterity/software/android-ndk-r10d/platforms/android-3/arch-arm/usr/include/linux/capability.h:
+
+/Users/posterity/software/android-ndk-r10d/platforms/android-3/arch-arm/usr/include/pathconf.h:
+
+/Users/posterity/software/android-ndk-r10d/platforms/android-3/arch-arm/usr/include/semaphore.h:
+
+/Users/posterity/software/android-ndk-r10d/platforms/android-3/arch-arm/usr/include/dirent.h:
+
+/Users/posterity/software/android-ndk-r10d/platforms/android-3/arch-arm/usr/include/pthread.h:
+
+/Users/posterity/software/android-ndk-r10d/platforms/android-3/arch-arm/usr/include/sched.h:
+
+/Users/posterity/Casey/qsa_code/qsaTalk/qsaTalk2/build/android/jni/../../..//include/libqsa_common.h:
+
+/Users/posterity/software/android-ndk-r10d/platforms/android-3/arch-arm/usr/include/stdlib.h:
+
+/Users/posterity/software/android-ndk-r10d/platforms/android-3/arch-arm/usr/include/alloca.h:
+
+/Users/posterity/software/android-ndk-r10d/platforms/android-3/arch-arm/usr/include/strings.h:
+
+/Users/posterity/software/android-ndk-r10d/platforms/android-3/arch-arm/usr/include/memory.h:
+
+/Users/posterity/Casey/qsa_code/qsaTalk/qsaTalk2/build/android/jni/../../..//include/libqsa_def.h:
+
+/Users/posterity/Casey/qsa_code/qsaTalk/qsaTalk2/build/android/jni/../../..//include/libqsa_callback.h:
