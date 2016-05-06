0a6fae8cef9b9adfc5c71dca9bcc2a76e8e03c39
Zhou Xiaomin <zxm123465@gmail.com>
2016-05-06 11:32:47 +0800

    modified:   talk.c

--- jni/talk.c
+++ jni/talk.c
@@ -344,7 +344,7 @@ void qsa_send_video(const char * data, int length, int frame_num, int frame_type
                 UdpSendBuff(m_VideoSocket, RemoteHost, RemoteVideoPort, 
                         mpeg4_out, (9 + sizeof(struct talkdata1) + talkdata.PackLen));
             }
-            //LOGD("%s:%d send_buf[61] = %d\n", __FUNCTION__, __LINE__, mpeg4_out[61]);
+            LOGD("%s:%d send_buf[61] = %d, PackLen = %d\n", __FUNCTION__, __LINE__, mpeg4_out[61], talkdata.PackLen);
         }
 #endif
 #else
